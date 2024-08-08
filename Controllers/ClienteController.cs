97bf09683737693ef839e994b180e4b475c1f404
Alexandre Dattilio <aledattilio@hotmail.com>
2024-08-08 16:36:01 -0300

    criando Controllers para Cliente e Produto

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExercicioProfMarcosGit.Controllers
+{
+    public class ClienteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
